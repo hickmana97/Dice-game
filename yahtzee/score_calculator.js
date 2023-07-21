f9b60ad826857e2805c56b49a832fcd330ca2816
hickmananwslc <132064235+hickmananwslc@users.noreply.github.com>
2023-07-21 10:40:21 +0100

    Update score_calculator.js
    Added counters for 3 of a kind, 4 of a kind and full house. This framework should allow me to calculate all possible scores

--- yahtzee/score_calculator.js
+++ yahtzee/score_calculator.js
@@ -1,17 +1,63 @@
 class Score {
   constructor(){
+    this.scoreReset()
+  }
+  
+  scoreReset(){
+    this.rollCountArray = []
     this.rollNumberArray = []
-    this.createScore()
+    this.score = 'ROLL TO SCORE'
+    this.oneCount = 0
+    this.twoCount = 0
+    this.threeCount = 0
+    this.fourCount = 0
+    this.fiveCount = 0
+    this.sixCount = 0
   }
 
   runScore(){
     for(let dice of diceArray){
       this.rollNumberArray.push(dice.number)
     }
-    console.log(this.rollNumberArray)
+    this.createScore()
   }
   
   createScore(){
-    
+    this.createCount()
+    for(let count of this.rollCountArray){
+      if(count === 3){
+        this.score = '3 OF A KIND'
+      }
+      else if(count === 4){
+        this.score = '4 OF A KIND'
+      }
+      else if(count === 5){
+        this.score = 'FULL HOUSE'
+      }
+    }
   }
-} 
+  
+  createCount(){
+    for(let number of this.rollNumberArray){
+      if(number === 1){
+        this.oneCount ++
+      }
+      if(number === 2){
+        this.twoCount ++
+      }
+      if(number === 3){
+        this.threeCount ++
+      }
+      if(number === 4){
+        this.fourCount ++
+      }
+      if(number === 5){
+        this.fiveCount ++
+      }
+      if(number === 6){
+        this.sixCount ++
+      }
+    }
+    this.rollCountArray.push(this.oneCount, this.twoCount, this.threeCount, this.fourCount, this.fiveCount, this.sixCount)
+  }
+}
